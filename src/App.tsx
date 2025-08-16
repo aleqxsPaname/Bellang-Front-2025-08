c2bcd7c92ae5667f249f511f64159210a6e7a311
alexisdebeauvais <alexisdebeauvais@gmail.com>
2025-08-16 17:44:47 +0200

    restructuration des dossiers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
-import Header from "./components/Header.tsx";
-import Sidebar from "./components/Sidebar.tsx";
-import FilterBar from "./components/FilterBar.tsx";
-import Player from "./components/Player.tsx";
-import Gallery from "./components/Gallery.tsx";
+import Header from "./components/header/Header.tsx";
+import Sidebar from "./components/sidebar/Sidebar.tsx";
+import FilterBar from "./components/filterBar/FilterBar.tsx";
+import Player from "./components/player/Player.tsx";
+import Gallery from "./components/gallery/Gallery.tsx";
 
 const App: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
